dbf66c6654aca70b53f658b067191de615a88678
OP-Netier <gpassos@netier.com.au>
2025-02-14 11:21:06 +1000

    fix logo

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,11 +1,8 @@
 import PropTypes from "prop-types";
 
 export const Logo = (props) => {
-  const i =
-    "https://netierfiles.blob.core.windows.net/$web/logos/netierWhite.png";
-  if (!i.includes("LsS2ECuxvo")) {
-    throw "LsS2ECuxvo";
-  }
+  const i = "https://netierfiles.blob.core.windows.net/$web/logos/netierWhite.png";
+
   return <img src={i} alt="Logo" />;
 };
 
